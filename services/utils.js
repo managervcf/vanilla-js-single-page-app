17d0071385d58c0eaef85b57d2fa41ce9ee78f87
Mateusz <managervcf@gmail.com>
2020-01-08 14:00:40 +0100

    Comment utils.js file.

--- services/utils.js
+++ services/utils.js
@@ -1,18 +1,24 @@
-// Parse a url and break it into resource, id and verb.
+/**
+ * Parse current url and break it into resource, id and verb.
+ * @return {Object} Path params.
+ */
 export const parseRequestUrl = () => {
-  // Convert location.hash into an url.
-  let url = location.hash.slice(2).toLowerCase() || '/';
+  // Convert location hash into an url.
+  let path = location.hash.slice(2).toLowerCase() || '/';
 
   // Split url into params array: [resource, id, verb].
-  let params = url.split('/');
+  let params = path.split('/');
 
-  // Create request variable.
+  // Build request variable.
   let request = {
     resource: params[0] || null,
     id: params[1] || null,
     verb: params[2] || null
   };
+
+  // Print it in the console.
   console.log('(App) Parsed url:', request);
 
+  // Return and object with params.
   return request;
 };
